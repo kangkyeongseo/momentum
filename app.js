6616ec5a8e03d9c34ff4775a63be75a821faafbd
kldream1204 <kks_big@naver.com>
2022-04-07 23:48:37 +0900

    Update Login Function

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector(".greeting");
+const greetingTitle = greeting.querySelector("h1");
+const logoutBtn = greeting.querySelector("button");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+
 function onLoginSubmit(event) {
     event.preventDefault();
+    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    localStorage.setItem(USERNAME_KEY, username);
+    loginInput.value = "";
     printGreeting();
 }
 
+function handleLogoutBtn() {
+    localStorage.removeItem(USERNAME_KEY);
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    readyForm();
+}
+
+function readyForm() {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+
 function printGreeting() {
-    const username = localStorage.getItem(USERNAME_KEY);
-    greeting.innerText =`Hello ${username}!`;
+    const localUsername = localStorage.getItem(USERNAME_KEY);
+    greetingTitle.innerText =`Hello ${localUsername}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    logoutBtn.addEventListener("click", handleLogoutBtn);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+    readyForm()
 }else {
     printGreeting();
 }
