13dd637b2afb3d59a2084f9c6080fda3cee3adb3
kldream1204 <kks_big@naver.com>
2022-04-08 15:39:19 +0900

    Random Background

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+
+const randomImage = images[Math.floor(Math.random()*images.length)];
+const bgImgae = document.createElement("img");
+ 
+bgImgae.src = `img/${randomImage}`;
+document.body.appendChild(bgImgae);
