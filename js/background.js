c8d6eccbe1cc2de6c467da75beb5f4a69f685a32
kldream1204 <kks_big@naver.com>
2022-04-08 17:32:59 +0900

    Update CSS

--- js/background.js
+++ js/background.js
@@ -2,6 +2,7 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const randomImage = images[Math.floor(Math.random()*images.length)];
 const bgImgae = document.createElement("img");
- 
+
+bgImgae.className = "background";
 bgImgae.src = `img/${randomImage}`;
 document.body.appendChild(bgImgae);
